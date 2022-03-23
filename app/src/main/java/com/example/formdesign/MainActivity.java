ef9c4987310068608d3d352ecbe910deb85488f0
MB <Meetbudheliya04@gmail.com>
2022-03-23 21:36:38 +0530

    23 March

--- app/src/main/java/com/example/formdesign/MainActivity.java
+++ app/src/main/java/com/example/formdesign/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn1 = findViewById(R.id.button1);
+        textView = findViewById(R.id.tv);
 
-        btn1.setText("Hello Devu");
+        btn1.setText("Hello Meet");
+        Intent i = getIntent();
+        String msg = i.getStringExtra("back_value");
+        textView.setText(msg);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn1.setText("Devu Clicked");
+                btn1.setText("Meet Button Clicked");
 
                 Intent i = new Intent(getApplicationContext(),SecondActivity.class);
 
--- app/src/main/java/com/example/formdesign/SecondActivity.java
+++ app/src/main/java/com/example/formdesign/SecondActivity.java
@@ -4,21 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView tv;
+    Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         tv = findViewById(R.id.text1);
+        back = findViewById(R.id.backbtn);
 
         Intent i = getIntent();
         String msg = i.getStringExtra("text");
         tv.setText(msg);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                i.putExtra("back_value","return from next");
+                startActivity(i);
+            }
+        });
 
     }
 }
